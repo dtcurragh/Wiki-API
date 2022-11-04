33070290c1e99fd1ee57403e169df95d20338c86
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2022-11-04 18:10:10 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
